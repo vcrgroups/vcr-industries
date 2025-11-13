75dba62b2a95477213501d747e1618cb3832aa66
vcrgroups <rajarahul.vcr@gmail.com>
2025-11-14 02:31:06 +0300

    Create Home component for VCR Group of Industries

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-5xl font-bold">VCR Group of Industries</h1>
+      <p className="text-xl mt-4">A multi-sector industrial group.</p>
+    </main>
+  );
+}
