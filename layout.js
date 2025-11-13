14dcd6d5d1d8348b2711cf9f020bdb42aa0300ca
vcrgroups <rajarahul.vcr@gmail.com>
2025-11-14 02:04:20 +0300

    Add files via upload

--- layout.js
+++ layout.js
@@ -1,1 +1,8 @@
-export default function RootLayout({children}){return(<html><body>{children}</body></html>)}
+export const metadata = { title: "VCR Group of Industries" };
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-50 text-gray-900">{children}</body>
+    </html>
+  );
+}
